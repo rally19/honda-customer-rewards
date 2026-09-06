f0306074ecb04473ec112e06f36aeffaf152bbae
rally19 <masterrally1808@gmail.com>
2026-09-06 22:35:46 +0700

    favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -37,9 +37,9 @@
             }
         </style>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" href="/images/logo/anper_logo_white.png">
+        <link rel="shortcut icon" type="image/png" href="/images/logo/anper_logo_white.png">
+        <link rel="apple-touch-icon" href="/images/logo/anper_logo_white.png">
 
         @fonts
 
