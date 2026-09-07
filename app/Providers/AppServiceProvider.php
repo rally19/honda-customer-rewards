87494084102d720fa13be2c74c9ce1b2e2ef4ad9
rally19 <masterrally1808@gmail.com>
2026-09-08 00:27:09 +0700

    pass min 8 karatarter

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -37,8 +37,9 @@ class AppServiceProvider extends ServiceProvider
             app()->isProduction(),
         );
 
-        Password::defaults(fn (): ?Password => app()->isProduction()
-            ? Password::min(12)
+        Password::defaults(
+            fn(): ?Password => app()->isProduction()
+            ? Password::min(8)
                 ->mixedCase()
                 ->letters()
                 ->numbers()
